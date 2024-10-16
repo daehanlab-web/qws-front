e86604f34e6a266e171e77fab2da388e9c01f79a
Daehan Won <daehan2lab@gmail.com>
2024-10-16 16:09:42 +0900

    lang setting

---
+++ lang/langpack_10162024.js
@@ -0,0 +1,21 @@
+en = {
+    'title_text_1': 'Services',
+    'title_text_2': 'Billing',
+    'title_text_3': 'Reference',
+    'title_text_4': 'Contact',
+    'title_text_5': 'Sign in',
+    'title_text_6': 'Sign up'
+}
+
+ko = {
+    'title_text_1': '서비스 및 앱 목록',
+    'title_text_2': '요금',
+    'title_text_3': '문서',
+    'title_text_4': '문의',
+    'title_text_5': '들어가기',
+    'title_text_6': '계정 생성'
+}
+
+lang_pack = {
+    'ko': ko, 'en': en
+}
